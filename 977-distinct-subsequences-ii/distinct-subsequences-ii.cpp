e78dada50c6c38e634143e5fe22067672098d717
Veerendranath sai Bhookya <bhookya.20243075@mnnit.ac.in>
2026-09-07 09:40:18 +0530

    Time: 2 ms (44.41%) | Memory: 8.6 MB (100.00%) - LeetSync

--- 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
@@ -4,3 +4,9 @@ public:
         const long long MOD = 1000000007;long long dp = 1;vector<long long> last(26, 0);for (char c : s) {int index = c - 'a';long long oldDp = dp;dp = (2 * dp - last[index] + MOD) % MOD;last[index] = oldDp;}return (dp - 1 + MOD) % MOD;
     }
 };
+
+auto speedup = []() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    return 0;
+}();
